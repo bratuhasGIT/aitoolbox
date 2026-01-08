fdaf7a9c0aab05d3cf553a306d757574ec0f081e
bratuhasGIT <kostaskpr@gmail.com>
2026-01-08 17:40:38 +0100

    Update site RSS config

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'AI tool box';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Practical AI prompts, tools, and workflows';
+export const SITE_URL = 'https://aitoolbox-ecj.pages.dev';
